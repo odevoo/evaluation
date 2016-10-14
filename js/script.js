843f963b8b8f165f43568eeec6bad2e3ea70addf
odevoo <elandry@odevoo.com>
2016-10-14 13:08:08 +0200

    push

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-$(document).ready(function(){
+
+$("#btn").on("click", function(){
+	var selectvalue = $("#select").val();
+	var areavalue = $("#textarea").val();
+	if (selectvalue == "") {
+		$("#select").addClass("red");
+	};
+	if (areavalue.length < 15) {
+		$("#textarea").addClass("red");
+	};
+});
+
+$("#textarea").change(function() {
+
+	$("#textarea").removeClass("red");
+});
 
 
+
+
+$("#select").change(function() {
+	$("#select").removeClass("red");
 });
